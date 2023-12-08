6d38ae06afd7e521c0bf02aa3b5a43620084b8a1
dontlookmes <phandinhhuy1216@gmail.com>
2023-12-08 09:33:28 +0900

    upload

--- data/api/no/as.php
+++
@@ -1,9 +0,0 @@
-<?php
-$str = '03432434234324234324';
-if (ctype_digit($str)) {
-    // Biến $loginInput là một số nguyên hợp lệ
-    echo "Số nguyên hợp lệ!";
-} else {
-    // Biến $loginInput không phải là một số nguyên hợp lệ
-    echo "Không phải là số nguyên hợp lệ!";
-}
